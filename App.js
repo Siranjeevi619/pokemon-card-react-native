12d467c2bdeb84277a22c19b0509f46a9345f770
Siranjeevi-Catom <siranjeevi0619@gmail.com>
2025-05-10 23:25:34 +0530

    fix:pokemon card fixed

--- App.js
+++ App.js
@@ -1,12 +1,4 @@
-import React from "react";
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar as RNStatusBar,
-  StyleSheet,
-} from "react-native";
-import Card from "./components/Card";
-import Pokemons from "./data/pokemon.json";
+
 import LoginPage from "./screens/login";
 import PokemonsList from "./screens/pokemonList";
 
--- screens/pokemonList/index.jsx
+++ screens/pokemonList/index.jsx
@@ -1,38 +1,45 @@
-import { ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { FlatList, StatusBar, StyleSheet, View } from "react-native";
 import React from "react";
 import Pokemons from "../../data/pokemon.json";
 import Card from "../../components/Card";
 import { styled } from "nativewind";
+
 const StyledView = styled(View);
-const StyleScrollView = styled(ScrollView);
 
 const PokemonsList = () => {
   const StatusBarIndicator = StatusBar.currentHeight;
+
   const styles = StyleSheet.create({
     container: {
       marginTop: StatusBarIndicator,
     },
   });
+
   return (
-    <ScrollView style={styles.container}>
-      <StyledView className="flex justify-center items-center">
-        <StyledView>
-          {Pokemons.map((pokemon, idx) => {
-            return (
-              <Card
-                name={pokemon.name}
-                type={pokemon.type}
-                hp={pokemon.hp}
-                attack={pokemon.attack}
-                speed={pokemon.speed}
-                image={pokemon.image}
-                key={idx}
-              />
-            );
-          })}
-        </StyledView>
-      </StyledView>
-    </ScrollView>
+    <StyledView
+      style={styles.container}
+      className="flex-1 items-center bg-white"
+    >
+      <FlatList
+        data={Pokemons}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <Card
+            name={item.name}
+            type={item.type}
+            hp={item.hp}
+            attack={item.attack}
+            speed={item.speed}
+            image={item.image}
+          />
+        )}
+        contentContainerStyle={{
+          paddingBottom: 20,
+          alignItems: "center",
+        }}
+        showsVerticalScrollIndicator={false}
+      />
+    </StyledView>
   );
 };
 
